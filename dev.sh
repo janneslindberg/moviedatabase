538095f8343323209e44d5581c080c7126709f0d
Janne Lindberg <janne.lindberg@gofore.com>
2023-02-07 15:04:43 +0200

    update

---
+++ dev.sh
@@ -0,0 +1,3 @@
+./mvnw clean spring-boot:run -Dspring-boot.run.profiles=dev -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8787"
+# -Dspring.profiles.active=test
+# -Dspring-boot.run.profiles
