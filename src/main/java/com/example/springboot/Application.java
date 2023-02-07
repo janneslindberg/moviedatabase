cad6dcfc6b2925120e55c357f948b996e39a9730
Janne Lindberg <janne.lindberg@gofore.com>
2023-02-07 10:49:14 +0200

    update

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -6,11 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+//		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
 //		System.out.println("Let's inspect the beans provided by Spring Boot:");
 //
--- src/test/java/my/moviedb/MovieControllerTest.java
+++ src/test/java/my/moviedb/MovieControllerTest.java
@@ -1,8 +1,19 @@
 package my.moviedb;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MovieControllerTest {
+	Logger log = LoggerFactory.getLogger(MovieControllerTest.class);
 
-	// TODO
-
+	
+	@Test
+	public void testingTest() {
+		log.error("Run this test for test purposes!");
+		
+	}
 }
